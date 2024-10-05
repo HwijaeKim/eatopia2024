e440099ea352643765bfe41b97d32fe57e0fde6e
HWIJAE KIM <dusan960766@gmail.com>
2024-10-05 11:07:31 +0900

    start

---
+++ script/index.js
@@ -0,0 +1,49 @@
+//index.html에 선언된 스크립트
+
+// let element = document.getElementById("element");
+
+new TypeIt("#element", {
+    lifeLike: false,
+    speed: 0,
+    loop: true
+})
+    .type("ㅇ")
+    .pause(80)
+    .delete(1)
+    .type("이")
+    .pause(97)
+    .delete(1)
+    .type("잇")
+    .pause(144)
+    .type("ㅌ")
+    .pause(53)
+    .delete(1)
+    .type("토")
+    .pause(75)
+    .delete(1)
+    .type("톺")
+    .pause(114)
+    .delete(1)
+    .type("토")
+    .pause(1)
+    .type("피")
+    .pause(46)
+    .delete(1)
+    .type("핑")
+    .pause(84)
+    .delete(1)
+    .type("피")
+    .pause(1)
+    .type("아")
+    .pause(1490)
+    .delete(1)
+    .type("ㅇ")
+    .pause(514)
+    .delete(1)
+    .pause(67)
+    .delete(1)
+    .pause(90)
+    .delete(1)
+    .pause(79)
+    .delete(1)
+    .go();
